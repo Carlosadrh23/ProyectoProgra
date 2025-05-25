477b278e3bdfbaf8dbf6f193016a0701ffccbc2b
David Gonzalez <davigonzalezdev@gmail.com>
2025-05-24 19:08:40 -0700

    fix en el login y se agrego un el textfield del usuario en el login

--- ProyectoRes/ProyectoRes/src/models/AuthModel.java
+++ ProyectoRes/ProyectoRes/src/models/AuthModel.java
@@ -17,42 +17,49 @@ public class AuthModel {
 
 	public AuthModel() {
 		
-	}
+	}	       
+
 	
-	public boolean login(String user,String password) {
-	    String query = "SELECT password FROM users WHERE username = ?";
-	    Connection conn = null;
-	    PreparedStatement stmt = null;
-	    ResultSet rs = null;
+	
+public boolean login(String username, String password) {
+	String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+    Connection conn = null;
+    PreparedStatement stmt = null;
+    ResultSet rs = null;
 
-	    try {
-	        Class.forName("com.mysql.cj.jdbc.Driver");
-	        conn = DriverManager.getConnection("jdbc:mysql://sql3.freesqldatabase.com/sql3781023", "sql3781023", "X9HKF7IsKB");
-	        stmt = conn.prepareStatement(query);
-	        stmt.setString(1, user); 
-	        
-	        rs = stmt.executeQuery();
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        conn = DriverManager.getConnection("jdbc:mysql://sql3.freesqldatabase.com/sql3781023", "sql3781023", "X9HKF7IsKB");
 
-	        if (rs.next()) {
-	       //	  String passwordHash = rs.getString("password");
-	       //       iad implementara al añadir el registro
-	            return true; 
-	        }
+        stmt = conn.prepareStatement(query);
+        stmt.setString(1, username);
+        stmt.setString(2, password);
 
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    } finally {
-	        try {
-	            if (rs != null) rs.close();
-	            if (stmt != null) stmt.close();
-	            if (conn != null) conn.close();
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	    }
+        rs = stmt.executeQuery();
 
-	    return false;
-	}
+        if (rs.next()) {
+            String storedHash = rs.getString("password");
+            
+            // Verificar la contraseña ingresada contra el hash almacenado
+                return true; // Login exitoso
+            
+        }
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    } finally {
+        try {
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    return false; // Login fallido
+}
+
 }
 		  
 
